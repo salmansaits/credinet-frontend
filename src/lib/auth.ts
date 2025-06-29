99ce30ac2352beaf54ab5a35729617ff8a6f737a
salmansaits <salmansaits@gmail.com>
2025-06-29 18:39:56 +0530

    Create auth.ts

---
+++ src/lib/auth.ts
@@ -0,0 +1,21 @@
+/// src/lib/auth.ts
+export function isAuthenticated() {
+  return !!localStorage.getItem("token")
+}
+
+/// src/lib/api.ts
+import axios from 'axios'
+
+const API = axios.create({
+  baseURL: 'https://credinet-backend.onrender.com',
+})
+
+API.interceptors.request.use(config => {
+  const token = localStorage.getItem('token')
+  if (token) {
+    config.headers.Authorization = `Bearer ${token}`
+  }
+  return config
+})
+
+export default API
